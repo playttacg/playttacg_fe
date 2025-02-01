77537e2c87c56bd76c7ad2c3f185f693d5e99a7a
truefuego <ankitsharma15092003@gmail.com>
2025-02-01 23:06:04 +0530

    Fix - http method context

--- src/context/httpContext.tsx
+++ src/context/httpContext.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, {
     createContext,
     useCallback,
@@ -38,6 +39,27 @@ export const HttpMethodContextProvider: React.FC<{children: React.ReactNode}> = 
             showLoader = true
         ): Promise<ApiResponseData<T>> => {
             if (showLoader) setShowApiLoader(true);
+
+            return axios.get(`${process.env.API_BASE_URL}/${endpoint}`)
+                .then((res) => {
+                    console.log(`🟢 Get: ${endpoint} ,${res.status}`);
+                    return {
+                        success: true,
+                        errorMsg: "",
+                        response: res as T,
+                    }
+                })
+                .catch(err => {
+                    console.log(`🔴 Get: ${endpoint} ,${err}`);
+                    return {
+                        success: false, 
+                        errorMsg: err, 
+                        response: {} as T
+                    }
+                })
+                .finally(() => {
+                    if (showLoader) setShowApiLoader(true);
+                })
         },[]
     );
 
