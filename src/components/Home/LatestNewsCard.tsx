def897cbe62ae85964cc0b052be31f28d0d49e97
truefuego <ankitsharma15092003@gmail.com>
2025-02-10 16:58:04 +0530

    fix - card and tailwind

--- src/components/Home/LatestNewsCard.tsx
+++ src/components/Home/LatestNewsCard.tsx
@@ -3,17 +3,17 @@ import pic from '../../assets/Screenshot 2025-01-18 222115 1.png'
 import { INewsCardProps } from "./type";
 const LatestNewsCard: React.FC<INewsCardProps> = ({ cardNewsData }) => {
   return (
-    <div className="max-w-[320px] rounded-2xl overflow-hidden shadow-md bg-white p-2 border">
-      <img className="w-[320px] rounded-xl" src={pic} alt={cardNewsData.title} />
-      <div className="mt-2">
-        <h2 className="text-sm font-semibold text-gray-800 truncate">{cardNewsData.title}</h2>
+    <div className="max-w-[350px] h-fit rounded-2xl overflow-hidden shadow-md bg-white">
+      <img className="w-[350px] h-[180px] object-cover" src={pic} alt={cardNewsData.title} />
+      <div className="p-2 flex justify-between item-center mb-1">
+        <h2 className="text-sm font-semibold text-gray-800 truncate pl-1">{cardNewsData.title}</h2>
         <a
           href={cardNewsData.slug}
           target="_blank"
           rel="noopener noreferrer"
-          className="text-blue-500 hover:underline mt-1 inline-block text-xs"
+          className="text-blue-500 hover:underline mt-1 mr-2 inline-block text-xs hover:underline-none"
         >
-          Learn More
+          &gt;
         </a>
       </div>
     </div>
