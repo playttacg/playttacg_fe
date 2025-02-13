677c20746006e00e12332d643090fb9021d66ea0
truefuego <ankitsharma15092003@gmail.com>
2025-02-13 15:14:05 +0530

    remove unused code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,11 @@
-import React, { useEffect } from "react";
-import useNewsApi from "../api/useNewsApi";
+import React from "react";
 import LatestNewsCard from "../components/Home/LatestNewsCard";
 import { latestNewsCardData } from "../constant/dummyConstant";
 import NavBar from "../components/common/NavBar";
 import BackgroundGradientLines from "../components/BackgroundGradientLines";
 
 const Home: React.FC = () => {
-  const { getAllNews } = useNewsApi();
-
-  useEffect(() => {
-    const fetchNews = async () => {
-      await getAllNews(true);
-    };
-    fetchNews();
-  }, [getAllNews]);
-
+  
   return (
     <div className="bg-[#0D1117] min-h-screen w-screen relative flex flex-col items-center z-10">
       <BackgroundGradientLines />
