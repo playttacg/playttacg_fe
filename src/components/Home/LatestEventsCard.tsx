3bf397a4da3c3273ef15d83d02d1f9502394e2a3
vishal2806 <vishalv2806@gmail.com>
2025-02-14 23:31:58 +0530

    newscard

---
+++ src/components/Home/LatestEventsCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LatestEventsCard = () => {
+  return (
+    <div>LatestEventsCard</div>
+  )
+}
+
+export default LatestEventsCard
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import LatestNewsCard from "../components/Home/LatestNewsCard";
-import { latestNewsCardData } from "../constant/dummyConstant";
+import LatestEventsCard from "../components/Home/LatestEventsCard";
 import NavBar from "../components/common/NavBar";
 import BackgroundGradientLines from "../components/BackgroundGradientLines";
+import useNewsApi from "../api/useNewsApi";
+
 
 const Home: React.FC = () => {
-  
+  const [newsData,setNewsData] = useState([]);
+  const {getAllNews} = useNewsApi();
+  const newFun = async()=>{
+    const getNews = await getAllNews();
+    const arrData = getNews.response.data.data;
+    setNewsData(arrData);
+  }
+  useEffect(()=>{newFun()},[])
+
   return (
     <div className="bg-[#0D1117] min-h-screen w-screen relative flex flex-col items-center z-10">
       <BackgroundGradientLines />
       <NavBar />
-      <div className="mt-48" />
-      <LatestNewsCard cardNewsData={latestNewsCardData} />
+      {/* show only 4 card data here just like in figma with 2 by 2 grid */}
+      <div className="mt-48 w-fit grid md:grid-cols-2 grid-cols-1 gap-x-8 gap-y-6" >
+      
+      {newsData.map((item, index) => (
+      <>
+        {index === 0 && <h3 className="text-amber-50 text-xl font-semibold mb-4 md:col-span-2">Latest News</h3>}
+        <LatestNewsCard key={index} cardNewsData={item} />
+      </>
+    ))}
+      </div>
     </div>
   );
 };
